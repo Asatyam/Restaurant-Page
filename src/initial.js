4b8a2daf15e1fb1c07c19e8687262712c7e32810
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-04-08 16:45:16 +0530

    Set up the template of the website

--- src/initial.js
+++ src/initial.js
@@ -10,8 +10,24 @@ const createHeading = () => {
   main.appendChild(headingDiv);
 };
 
+const createBtn = (tabsDiv, btnName) => {
+  const btn = document.createElement('button');
+  btn.setAttribute('class', btnName);
+  btn.textContent = btnName.toUpperCase();
+  tabsDiv.appendChild(btn);
+};
+
+const createTabs = () => {
+  const tabsDiv = document.createElement('div');
+  createBtn(tabsDiv, 'home');
+  createBtn(tabsDiv, 'menu');
+  createBtn(tabsDiv, 'contact');
+  tabsDiv.setAttribute('class', 'tabs');
+  main.appendChild(tabsDiv);
+};
+
 function createWebsite() {
   createHeading();
-  console.log('working');
+  createTabs();
 }
 export { createWebsite };
